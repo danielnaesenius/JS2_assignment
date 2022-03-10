f14b7f33e5aaad2577d3178a9bcf883eb67e44ef
Daniel Naesenius <daniel.naesenius@gmail.com>
2022-03-10 22:13:19 +0100

    created navbar + added icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Navbar.css";
+import { GrHomeRounded, GrContactInfo } from "react-icons/gr";
+import { BsPersonCircle } from "react-icons/bs";
+import { BiBriefcaseAlt2 } from "react-icons/bi";
 
 function Navbar() {
   return (
@@ -8,25 +11,25 @@ function Navbar() {
         <div className="home nav-link">
           <a href="#">
             Home
-            <img src="" alt="icon" />
+            <GrHomeRounded />
           </a>
         </div>
         <div className="about nav-link">
           <a href="#">
             About
-            <img src="" alt="icon" />
+            <BsPersonCircle />
           </a>
         </div>
         <div className="projects nav-link">
           <a href="#">
             Projects
-            <img src="" alt="icon" />
+            <BiBriefcaseAlt2 />
           </a>
         </div>
         <div className="contact nav-link">
           <a href="#">
             Contact
-            <img src="" alt="icon" />
+            <GrContactInfo />
           </a>
         </div>
       </div>
