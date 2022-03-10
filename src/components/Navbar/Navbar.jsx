92138c3b8050168c2b83ffdf50937c2b3008da6e
Daniel Naesenius <daniel.naesenius@gmail.com>
2022-03-10 21:42:35 +0100

    Styling navbar, adding links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <div>
-        <div className="navContainer"></div>
+      <div className="navContainer">
+        <div className="home nav-link">
+          <a href="#">
+            Home
+            <img src="" alt="icon" />
+          </a>
+        </div>
+        <div className="about nav-link">
+          <a href="#">
+            About
+            <img src="" alt="icon" />
+          </a>
+        </div>
+        <div className="projects nav-link">
+          <a href="#">
+            Projects
+            <img src="" alt="icon" />
+          </a>
+        </div>
+        <div className="contact nav-link">
+          <a href="#">
+            Contact
+            <img src="" alt="icon" />
+          </a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
