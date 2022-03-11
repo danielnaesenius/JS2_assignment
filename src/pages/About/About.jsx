9777833aa527109ec7406d20776be91b82ed2923
Daniel Naesenius <daniel.naesenius@gmail.com>
2022-03-11 22:50:35 +0100

    Working on about-page and CV

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./About.css";
 import pic from "../../resources/avatar.svg";
+import content from "../../resources/aboutText.js";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { FiTwitter } from "react-icons/fi";
 
-function About() {
+function About({ title, location, text }) {
   return (
     <div className="about-page" id="about">
       <div className="title">
@@ -11,6 +14,29 @@ function About() {
       <div className="about-me">
         <div className="about-img-container">
           <img src={pic} alt="Daniel Profile Picture" />
+        </div>
+        <div className="text-container">
+          <h2>{content["title"]}</h2>
+          <p className="bold">{content["location"]}</p>
+          <p>{content["text"]}</p>
+          {content["skills"].map((skill) => {
+            console.log(skill);
+            return <li>{skill}</li>;
+          })}
+          <p>{content["end"]}</p>
+        </div>
+        <div className="icon-container">
+          <a href="https://twitter.com/?lang=en" target="_blank">
+            <FiTwitter className="icon-media-about" />
+          </a>
+
+          <a href="https://github.com/danielnaesenius" target="_blank">
+            <AiFillGithub className="icon-media-about" />
+          </a>
+
+          <a href="https://www.linkedin.com/" target="_blank">
+            <AiFillLinkedin className="icon-media-about" />
+          </a>
         </div>
       </div>
     </div>
---
+++ src/resources/aboutText.js
@@ -0,0 +1,8 @@
+const textContent = {
+  title: "Daniel N. 27 years old.",
+  location: "Stockholm, Sweden",
+  text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id quibusdam alias laudantium! Quam minima nulla voluptas dolore laborum perspiciatis, qui vitae ducimus possimus quis eos maiores dicta, expedita ut, laudantium sed velit? Incidunt quos atque aperiam quis obcaecati eligendi provident ad vero esse minus, similique nam inventore reprehenderit facere laboriosam?",
+  skills: ["React", "HTML & CSS", "JavaScript", "NodeJS"],
+  end: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id quibusdam alias laudantium! Quam minima nulla voluptas dolore laborum perspiciatis.",
+};
+export default textContent;
